a52d5138c4c3064df8b0fd316aa717b16abdf053
mycomycul <32719514+mycomycul@users.noreply.github.com>
2018-12-31 13:11:47 -0800

    Optimize Prime Factor Method

--- Euler/primefactor.js
+++ Euler/primefactor.js
@@ -1,39 +1,23 @@
-factor(600851475143);
+factor(6008514751431234);
 function factor(number) {
     if (validateInput(number)) {
         var resultText = "Prime Factors are: ";
-        //Find Factors
-        for (var x = 2; x <= number / 2; x += 1) {
-            if (x % 2 != 0) {
-                var prime = false;
-                if (number % x == 0) {
-                    prime = checkforprime(x);
-
-                    //If it is a prime factor
-                    if (prime == true)
-                        resultText += x + ",";
-                }
+        p = 2;
+        while(number >= p*p){
+            if(number%p == 0){
+                number = number/p;
+                resultText += p + ","
+            }
+            else{
+                p++;
             }
         }
+        resultText += number;
 
-        resultText = resultText.slice(0, -1);
         console.log(resultText);
         // document.getElementById('result').innerHTML = resultText;
     }
     else return;
-}
-
-function checkforprime(int) {
-    for (var y = 2; y <= int / 2; y++) {
-        if (int % y == 0) {
-            return false;
-        }
-    }
-    //no factor of the number was found so it's prime
-    // document.getElementById('result').innerHTML = "Current Number: " + int;
-    console.log("Current Number: " + int);
-
-    return true;
 }
 
 
